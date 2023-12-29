6afda6be1c152abfd00ce56420b467488642ecd2
Genetics <andremueller562@gmail.com>
2023-12-29 19:16:10 +0100

    Fix some issues in battle and movement command handler

--- src/trading/external/handler/battle_command_handler.rs
+++ src/trading/external/handler/battle_command_handler.rs
@@ -3,6 +3,7 @@ use std::sync::Arc;
 use rayon::prelude::*;
 use tokio::sync::Mutex;
 use tracing::error;
+use tracing::log::info;
 use uuid::Uuid;
 use crate::game::game_state::GameState;
 use crate::player::PlayerState;
@@ -19,34 +20,31 @@ pub struct DamageReport {
 
 pub async fn calculate_damage_for_round(game_state: &mut GameState) -> Vec<DamageReport> {
     let round_state = game_state.round_states.get_mut(&game_state.current_round).unwrap();
-    let player_states = &round_state.player_name_player_map;
 
-    player_states.par_iter().flat_map(|(_, player)| {
-        let mut local_damage_reports = Vec::new();
-        let commands = player.commands.get(&CommandType::BATTLE).expect("Battle commands are required");
+    round_state.player_name_player_map.iter_mut().flat_map(|(_, player)| {
+        player.commands.get_mut(&CommandType::BATTLE).into_iter().flat_map(|commands| {
+            commands.drain(..).filter_map(|command| {
+                let attacker_id = command.command_object.robot_id.expect("Attacker id is required");
+                let target_id = command.command_object.target_id.expect("Target id is required");
 
-        for command in commands {
-            let attacker_id = command.command_object.robot_id.expect("Attacker id is required");
-            let target_id = command.command_object.target_id.expect("Target id is required");
-
-            if let Some(attacker_robot) = player.robots.get(&attacker_id) {
-                if attacker_robot.is_alive() {
-                    let attacker_damage = attacker_robot.levels.get_damage_for_level();
-                    local_damage_reports.push(DamageReport {
-                        attacker_id,
-                        attacker_name: player.player_name.clone(),
-                        defender_id: target_id,
-                        damage_to_take: attacker_damage,
-                    });
-                } else {
-                    error!("Attacker robot of {} with id {} is dead it cannot attack", &player.player_name, &attacker_id);
-                }
-            }
-        }
-        local_damage_reports
+                player.robots.get(&attacker_id).and_then(|attacker_robot| {
+                    if attacker_robot.is_alive() {
+                        let attacker_damage = attacker_robot.levels.get_damage_for_level();
+                        Some(DamageReport {
+                            attacker_id,
+                            attacker_name: player.player_name.clone(), // consider if clone is necessary here
+                            defender_id: target_id,
+                            damage_to_take: attacker_damage,
+                        })
+                    } else {
+                        error!("Attacker robot of {} with id {} is dead; it cannot attack", &player.player_name, &attacker_id);
+                        None
+                    }
+                })
+            })
+        })
     }).collect()
 }
-
 pub fn apply_damage_for_round(damage_reports: Vec<DamageReport>, game_state: &mut GameState) {
     pub struct KillReport {
         pub attacker_robot_id: Uuid,
@@ -56,21 +54,28 @@ pub fn apply_damage_for_round(damage_reports: Vec<DamageReport>, game_state: &mu
     }
     let mut kill_reports = Vec::new();
     for damage_report in damage_reports {
-        let (target_robot, target_player_name) = game_state.get_robot_and_playername_for_current_round_by_robot_id(&damage_report.defender_id).expect("Target robot not found");
-        target_robot.take_damage(damage_report.damage_to_take);
-        if !target_robot.is_alive() {
-            kill_reports.push(KillReport {
-                attacker_name: damage_report.attacker_name,
-                attacker_robot_id: damage_report.attacker_id,
-                player_name_whose_robot_got_killed: target_player_name,
-                killed_robot: target_robot.clone()
-            });
+        if let Some((target_robot, target_player_name)) = game_state.get_robot_and_playername_for_current_round_by_robot_id(&damage_report.defender_id) {
+            target_robot.take_damage(damage_report.damage_to_take);
+            if !target_robot.is_alive() {
+                info!("Robot {} of player {} was killed by {}", damage_report.defender_id, target_player_name, damage_report.attacker_name);
+                kill_reports.push(KillReport {
+                    attacker_name: damage_report.attacker_name.clone(),
+                    attacker_robot_id: damage_report.attacker_id,
+                    player_name_whose_robot_got_killed: target_player_name.clone(),
+                    killed_robot: target_robot.clone() // Consider avoiding clone
+                });
+            }
+        } else {
+            error!("Target robot not found for ID {}", damage_report.defender_id);
         }
     }
 
     for report in kill_reports {
-        let player_state = game_state.get_player_for_current_round_as_mut(&report.attacker_name).expect(format!("Player {} (Attacker) not found", report.attacker_name).as_str());
-        player_state.killed_robots.insert(report.attacker_robot_id, (report.player_name_whose_robot_got_killed, report.killed_robot));
+        if let Some(player_state) = game_state.get_player_for_current_round_as_mut(&report.attacker_name) {
+            player_state.killed_robots.insert(report.attacker_robot_id, (report.player_name_whose_robot_got_killed, report.killed_robot));
+        } else {
+            error!("Player {} (Attacker) not found", report.attacker_name);
+        }
     }
 }
 
@@ -83,13 +88,17 @@ pub fn delete_commands_for_dead_robots(game_state: &mut GameState) {
         //clear mining and regenerating commands for dead robots
         for (robot_id, robot) in players_robots.iter_mut() {
             if !robot.is_alive() {
-                let mining_commands = player.commands.get_mut(&CommandType::MINING).expect("Mining commands are required");
-                mining_commands.retain(|command| command.command_object.robot_id.unwrap() != *robot_id);
-
-                let regenerating_commands = player.commands.get_mut(&CommandType::REGENERATE).expect("Regenerating commands are required");
-                regenerating_commands.retain(|command| command.command_object.robot_id.unwrap() != *robot_id);
+                if let Some(mining_commands) = player.commands.get_mut(&CommandType::MINING) {
+                    mining_commands.retain(|command| command.command_object.robot_id.unwrap() != *robot_id);
+                } else {
+                    error!("Mining commands are required");
+                }
+                if let Some(regenerating_commands) = player.commands.get_mut(&CommandType::REGENERATE) {
+                    regenerating_commands.retain(|command| command.command_object.robot_id.unwrap() != *robot_id);
+                } else {
+                    error!("Regenerating commands are required");
+                }
             }
         }
-
     }
 }
--- src/trading/external/handler/movement_command_handler.rs
+++ src/trading/external/handler/movement_command_handler.rs
@@ -10,33 +10,41 @@ use crate::trading::external::command_type::CommandType;
 pub fn handle_movement_commands(game_state: &mut GameState) {
     let round_state = game_state.round_states.get_mut(&game_state.current_round).unwrap();
     let map = &round_state.map;
-    let player_name_player_map = round_state.player_name_player_map.values_mut();
-    for player in player_name_player_map {
+
+    for player in round_state.player_name_player_map.values_mut() {
         if let Some(movement_commands_queue) = player.commands.get_mut(&CommandType::MOVEMENT) {
             while let Some(movement_command) = movement_commands_queue.pop_front() {
                 let robot_id = movement_command.command_object.robot_id.expect("Robot id is required");
                 let target_planet_id = movement_command.command_object.target_id.expect("Target id is required");
                 let robot = player.robots.get_mut(&robot_id).expect("Robot not found");
-                let current_planet_mov_difficulty = map.get_planet(&robot.planet_id).expect("Current planet not found").movement_difficulty;
-                let target_planet = map.get_planet(&target_planet_id).expect("Target planet not found");
-                if robot.is_alive() {} else {
+
+                if !robot.is_alive() {
                     info!("Robot {} is dead and cannot move", robot_id);
-                    if robot.planet_id == target_planet_id {
-                        info!("Robot {} is already on planet {}", robot_id, target_planet_id);
-                    } else if target_planet.neighbours.values().any(|neighbour_id| neighbour_id == &robot.planet_id) {
-                        if robot.energy < current_planet_mov_difficulty as u32 {
-                            info!("Robot {} cannot move to planet {} because it does not have enough energy", robot_id, target_planet_id);
-                            continue;
-                        }
-                        robot.energy -= current_planet_mov_difficulty as u32;
-                        robot.planet_id = target_planet_id;
-                        info!("Robot {} moved to planet {}", robot_id, target_planet_id);
-                        if player.visited_planets.insert(target_planet_id) {
-                            info!("Player {} just discovered planet {}", player.player_name, target_planet_id);
-                        }
-                    } else {
-                        info!("Robot {} cannot move to planet {} because it is not a neighbour", robot_id, target_planet_id);
-                    }
+                    continue;
+                }
+
+                if robot.planet_id == target_planet_id {
+                    info!("Robot {} is already on planet {}", robot_id, target_planet_id);
+                    continue;
+                }
+
+                let current_planet = map.get_planet(&robot.planet_id).expect("Current planet not found");
+                if !current_planet.neighbours.values().any(|neighbour_id| neighbour_id == &target_planet_id) {
+                    info!("Robot {} cannot move to planet {} because it is not a neighbour", robot_id, target_planet_id);
+                    continue;
+                }
+
+                if robot.energy < current_planet.movement_difficulty as u32 {
+                    info!("Robot {} cannot move to planet {} because it does not have enough energy", robot_id, target_planet_id);
+                    continue;
+                }
+
+                robot.energy -= current_planet.movement_difficulty as u32;
+                robot.planet_id = target_planet_id;
+                info!("Robot {} moved to planet {}", robot_id, target_planet_id);
+
+                if player.visited_planets.insert(target_planet_id) {
+                    info!("Player {} just discovered planet {}", player.player_name, target_planet_id);
                 }
             }
         }
