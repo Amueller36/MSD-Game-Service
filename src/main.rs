7408c4d6f22ad27589c07dad9bf09cf85ec3b37c
Genetics <andremueller562@gmail.com>
2023-12-29 17:52:28 +0100

    Added Cors to allow every resource

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,7 @@ async fn main() -> Result<(),std::io::Error>{
     HttpServer::new(move || {
         actix_web::App::new()
             .wrap(
-                Cors::permissive().send_wildcard().allowed_origin("http://localhost:4200")
+                Cors::permissive().allowed_origin("http://localhost:4200")
             )
             .app_data(Data::clone(&pool_as_sharable_data))
             .service(hello_world)
